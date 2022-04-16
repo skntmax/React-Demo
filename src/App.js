0cbc06f5843d3052cb5f45e4632283261e3d98f0
Shashi Kant <shashi.kant@valueinnovationlabs.com>
2022-04-16 22:01:36 +0530

    use context hook example

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
+import React ,{createContext ,useState} from 'react'
 import './App.css';
 import './asssets/style.css'
 import SwitchButton from './components/Switch';
+import CustomizedSelects from './components/CustomizedSelects'
+import Accordian from './components/Accordian';
+import Backdropcmp from './components/Backdropcmp';
+import Skeletoncmp from './components/Skeleton';
+import FileUpload from './components/FileUpload';
+import ComponentA from './components/ComponentA';
+ 
+export const UserContext = createContext()
+
 function App() {
+  //  const userContext = useContext(null)
+  const [user, setUser] = useState("Jesse Hall");
   return (
     <div className="parentDiv">
-        <SwitchButton />
+     
+        { /* <SwitchButton />  */ }
+       { /* <CustomizedSelects /> */ }         
+       { /* <Accordian /> */ }
+       { /* <Backdropcmp />  */ } 
+        
+      { /* <Skeletoncmp />  */}
+        
+       { /* <FileUpload />  */}  
+          
+        
+
+       <UserContext.Provider value={user} >
+             <ComponentA user2={user}  />
+       </UserContext.Provider>
+        
+
+
+
+
     </div>
   );
 }
