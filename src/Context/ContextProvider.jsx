8e9c92fe2231c242c5d6b842c23dd04477422be1
Shashi Kant <skntjee@gmail.com>
2022-11-20 23:55:52 +0530

    todo list modified

--- src/Context/ContextProvider.jsx
+++ src/Context/ContextProvider.jsx
@@ -2,9 +2,8 @@ import React, { createContext } from 'react'
 export let context = createContext()
 
 function ContextProvider({children}) {
-    
     return (
-    <context.Provider value="idk" >
+    <context.Provider value=" dafualt value " >
       {children} 
       </context.Provider>
   )
--- src/component/Todo.jsx
+++ src/component/Todo.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react"
 import './../assets/style.css'
 export default function Todo() {
+      
   const [prdt ,setProduct]  = useState('')
   const [list ,setList]  = useState([])
   const [edit ,editId]  = useState(undefined)
@@ -12,33 +13,85 @@ export default function Todo() {
     else {
         editId(undefined)        
     } 
+      
    }
     
    const saveChanges =()=>{
          list[edit] = editedValue
           setList([...list])
-    }
+     }
+
+     const addList = ()=>{
+          if(prdt!="") {
+               setList([...list,prdt ] ,()=>{
+                     console.log(" log updated ");
+               })
+                   let final_list = [...list, prdt]
+                    localStorage.setItem('item' ,JSON.stringify(final_list) ) 
+                     setProduct("")
+          } else 
+          alert("please insert something ") 
+          
+     }
    
+    React.useEffect((item) => {
+
+      if(localStorage.getItem('item')) {
+           setList(JSON.parse(localStorage.getItem('item')))
+        }
+    
+           
+    }, []) ;
+
+     
     return (
          <React.Fragment>
           <div className="main_div">
           
+
+         
+
            <div> 
            <h1> Todo -list </h1>
-           <input   onChange={e=> setProduct(e.target.value)} value={prdt} />
-            <button onClick={e=> setList([...list,prdt ]) }> save me </button>
+
+           <div className="form-group">
+          <textarea onChange={e=> setProduct(e.target.value)} value={prdt} className="form-control rounded-0" id="exampleFormControlTextarea1" rows="10"></textarea>
+          </div>
+          <button onClick={e=> addList() } type="button" className="btn btn-outline-success btn-block my-2"> save me </button>
+          <div class="d-flex flex-row flex-wrap">
              
             {list && list.length>0 && 
             list.map((ele ,index)=>{
                  return (
                      <React.Fragment>
-                      <li> {index+">"} {ele} <button onClick={e=> setList(  list.filter((ele,innerIndex) => innerIndex!=index )  ) } > delete </button>   
-                     <button onClick={e=> editItem(ele, index ,e )} > Edit  </button>  </li> 
+
+
+                     <div class="p-2">
+                             <div class="md-form mb-4 pink-textarea active-pink-textarea-2">
+                             <textarea 
+                             value={ele}
+                             id="form17" class="md-textarea form-control" rows="3"></textarea>
+                        
+                             </div> 
+
+                     <button  className="btn btn-light" onClick={e=> setList(  list.filter((ele,innerIndex) => innerIndex!=index )  ) } > delete </button>   
+                     <button className="btn btn-light" onClick={e=> editItem(ele, index ,e )} > Edit  </button>  
+                    </div>
+                     
+
+
+                 
+
+
+
+
                      {editId!=undefined &&  index==edit && <input onChange={e=> setEditValue(e.target.value) } placeholder={list[edit]}   />}
                      {edit!=undefined && index==edit && <button onClick={saveChanges}> save changes </button>}
                      </React.Fragment>
                   )
             })}   
+            </div>
+
              
             </div>
         
